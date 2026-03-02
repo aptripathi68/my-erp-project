0f10e2231cb9a47c2dfcf57358a5b8f6dd6c7983
Arun Pati Tripathi <aruntripathi@Aruns-MacBook-Air.local>
2026-03-02 11:34:06 +0530

    Add root URL home page

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,16 @@
+python
+
 """
 URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+# Simple home view
+def home(request):
+    return HttpResponse("""
+        <h1>ERP System</h1>
+        <p>Welcome to your inventory management system.</p>
+        <p><a href="/admin/">Go to Admin</a></p>
+    """)
