a7f68c43ea86a5cb18fab151d072c6f5b3f5e033
Arun Pati Tripathi <aruntripathi@Aruns-MacBook-Air.local>
2026-03-06 02:31:44 +0530

    Improve Item Master admin grid editing

--- masters/admin.py
+++ masters/admin.py
@@ -1,66 +1,119 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django import forms
 from .models import Group2, Grade, Item
-
-
-class ExcelImportForm(forms.Form):
-    excel_file = forms.FileField(
-        label='Select Excel File',
-        help_text='Upload Excel file with Group2, Grade, Item Code, Description columns'
-    )
-    
-    class Meta:
-        widgets = {
-            'excel_file': forms.FileInput(attrs={'accept': '.xlsx,.xls'})
-        }
 
 
 @admin.register(Group2)
 class Group2Admin(admin.ModelAdmin):
-    list_display = ['code', 'name', 'grade_count', 'item_count']
-    search_fields = ['code', 'name']
-    
+    list_display = ["code", "name", "grade_count", "item_count"]
+    search_fields = ["code", "name"]
+
     def grade_count(self, obj):
         return obj.grades.count()
-    grade_count.short_description = 'Grades'
-    
+    grade_count.short_description = "Grades"
+
     def item_count(self, obj):
         return Item.objects.filter(group2=obj).count()
-    item_count.short_description = 'Items'
+    item_count.short_description = "Items"
 
 
 @admin.register(Grade)
 class GradeAdmin(admin.ModelAdmin):
-    list_display = ['code', 'name', 'group2', 'item_count']
-    list_filter = ['group2']
-    search_fields = ['code', 'name']
-    
+    list_display = ["code", "name", "group2", "item_count"]
+    list_filter = ["group2"]
+    search_fields = ["code", "name"]
+
     def item_count(self, obj):
         return obj.items.count()
-    item_count.short_description = 'Items'
+    item_count.short_description = "Items"
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['item_master_id', 'item_description', 'group2', 'grade', 'unit_weight', 'is_active']
-    list_filter = ['group2', 'grade', 'is_active', 'import_batch_id']
-    search_fields = ['item_master_id', 'item_description', 'hsn_code']
-    list_editable = ['is_active']
-    
-    def get_urls(self):
-        urls = super().get_urls()
-        custom_urls = [
-            path('import-excel/', self.admin_site.admin_view(self.import_excel), name='masters_item_import'),
-        ]
-        return custom_urls + urls
-    
-    def import_excel(self, request):
-        from django.http import HttpResponse
-        return HttpResponse("Import page is working! Your template issue is fixed.")
+    """
+    Item Master Online Grid:
+    - List view shows many rows (like Excel)
+    - Inline edit for safe fields
+    - Full edit still available by clicking item_master_id
+    - Add new item still available from top-right "Add Item"
+    """
+
+    list_display = [
+        "item_master_id",
+        "item_description",
+        "group2",
+        "grade",
+        "unit_weight",
+        "unit_weight_basis",
+        "is_active",
+        "updated_at",
+    ]
+
+    # Inline editable fields in list view (safe operations)
+    # NOTE: Django rule: first column of list_display cannot be editable.
+    list_editable = [
+        "group2",
+        "grade",
+        "unit_weight",
+        "unit_weight_basis",
+        "is_active",
+    ]
+
+    list_filter = [
+        "group2",
+        "grade",
+        "unit_weight_basis",
+        "is_active",
+    ]
+
+    search_fields = [
+        "item_master_id",
+        "item_description",
+        "item_description_norm",
+        "hsn_code",
+    ]
+
+    ordering = ["item_description"]
+    list_per_page = 50
+    save_on_top = True  # shows Save buttons at top also
+
+    # Optional: make it harder to accidentally delete
+    actions = None
+
+    # Optional: show read-only fields in the edit form (detail page)
+    readonly_fields = ["item_description_norm", "created_at", "updated_at"]
+
+    fieldsets = (
+        ("Basic", {
+            "fields": (
+                "item_master_id",
+                "item_description",
+                "item_description_norm",
+                "group2",
+                "grade",
+                "is_active",
+            )
+        }),
+        ("Weight", {
+            "fields": (
+                "unit_weight",
+                "unit_weight_basis",
+            )
+        }),
+        ("Tax / Optional", {
+            "classes": ("collapse",),
+            "fields": (
+                "hsn_code",
+                "tax_rate",
+                "group1_name",
+                "section_name",
+                "import_batch_id",
+            )
+        }),
+        ("Timestamps", {
+            "classes": ("collapse",),
+            "fields": (
+                "created_at",
+                "updated_at",
+            )
+        }),
+    )
