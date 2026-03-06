bb53b4b128ba89ca5c307fc9152e502e1f0964dc
Arun Pati Tripathi <aruntripathi@Aruns-MacBook-Air.local>
2026-03-06 13:44:30 +0530

    Serve static and media files in debug mode

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from config.views import dashboard_home
 
-
 urlpatterns = [
-    # ERP Dashboard (main landing page)
     path("", dashboard_home, name="dashboard_home"),
-
-    # Django Admin (for maintenance)
     path("admin/", admin.site.urls),
-
-    # Masters APIs
     path("api/masters/", include("masters.urls")),
-
-    # Ledger APIs
     path("api/", include("ledger.urls")),
-
-    # Procurement APIs
     path("procurement/", include("procurement.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
