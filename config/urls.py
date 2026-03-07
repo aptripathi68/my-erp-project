ad6b9c342c3ac6c8d04b391c76712362c17f9ee9
Arun Pati Tripathi <aruntripathi@Aruns-MacBook-Air.local>
2026-03-07 14:07:42 +0530

    Update ERP deployment routing

--- config/urls.py
+++ config/urls.py
@@ -8,10 +8,8 @@ from config.views import dashboard_home
 urlpatterns = [
     path("", dashboard_home, name="dashboard_home"),
     path("admin/", admin.site.urls),
-
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-
     path("", include("masters.urls")),
     path("api/", include("ledger.urls")),
     path("procurement/", include("procurement.urls")),
