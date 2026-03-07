c0aacb30efbd5f398d635a920a3f678f2e4eea7b
Arun Pati Tripathi <aruntripathi@Aruns-MacBook-Air.local>
2026-03-07 09:07:54 +0530

    Added login/logout URLs and template for /erp subpath

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views  # ADD THIS IMPORT
 from config.views import dashboard_home
 
 urlpatterns = [
     path("", dashboard_home, name="dashboard_home"),
     path("admin/", admin.site.urls),
+
+    # ADD THESE TWO LINES for login/logout
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     # ERP pages
     path("", include("masters.urls")),
